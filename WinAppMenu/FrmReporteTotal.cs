a06b452b136f962314ddd6ea93ca81b6ac5f711e
Alejandro2025RK <mc.nekross556gmail.com>
2025-05-15 08:43:56 -0500

    Cambios finales

--- WinAppMenu/FrmReporteTotal.cs
+++ WinAppMenu/FrmReporteTotal.cs
@@ -1,26 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using Microsoft.Reporting.WinForms;
 
 namespace WinAppMenu
 {
     public partial class FrmReporteTotal : Form
     {
+        DataTable mochilasTable;
+
         public FrmReporteTotal()
         {
             InitializeComponent();
+            string rutaXml = Path.Combine(Application.StartupPath, "XML", "Mochilas.xml");
+
+            // Carga el XML en el DataSet (asegúrate que dataSet1 está declarado en el diseñador)
+            dataSet1.ReadXml(rutaXml);
+
+            mochilasTable = dataSet1.Tables[0]; // Guarda la tabla principal
+
+            // Configura el reporte con la tabla completa (sin filtro)
+            Microsoft.Reporting.WinForms.ReportDataSource rds =
+                new Microsoft.Reporting.WinForms.ReportDataSource("DataSet1", mochilasTable);
+
+            reportViewer1.LocalReport.DataSources.Clear();
+            reportViewer1.LocalReport.DataSources.Add(rds);
+
+            // Establece el nombre del archivo .rdlc (ajusta ruta/nombre si es necesario)
+            reportViewer1.LocalReport.ReportEmbeddedResource = "WinAppMenu.ReporteTotal.rdlc";
         }
 
         private void FrmReporteTotal_Load(object sender, EventArgs e)
         {
+            reportViewer1.RefreshReport();
+        }
 
-            this.reportViewer1.RefreshReport();
+        private void reportViewer1_Load(object sender, EventArgs e)
+        {
+            // Opcionalmente puedes dejarlo vacío o poner lógica aquí
         }
     }
 }
