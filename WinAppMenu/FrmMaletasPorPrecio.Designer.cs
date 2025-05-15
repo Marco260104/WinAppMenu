c36f6372e4373a8f1dd9ab93c043195da579eb87
HectorGrayed <hector.puenayan@espoch.edu.ec>
2025-05-14 23:31:34 -0500

    Reporte por precio

--- WinAppMenu/FrmMaletasPorPrecio.Designer.cs
+++ WinAppMenu/FrmMaletasPorPrecio.Designer.cs
@@ -28,19 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMaletasPorPrecio));
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataSet1 = new WinAppMenu.DataSet1();
+            this.tblDatosBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblDatosBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(52, 19);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Location = new System.Drawing.Point(46, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(123, 135);
+            this.pictureBox1.Size = new System.Drawing.Size(109, 108);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
@@ -50,23 +60,90 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Berlin Sans FB", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(41)))), ((int)(((byte)(110)))));
-            this.label1.Location = new System.Drawing.Point(266, 41);
+            this.label1.Location = new System.Drawing.Point(352, 27);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(396, 52);
+            this.label1.Size = new System.Drawing.Size(329, 42);
             this.label1.TabIndex = 4;
             this.label1.Text = "Mochilas por Precio";
             // 
+            // reportViewer1
+            // 
+            reportDataSource2.Name = "DataSet1";
+            reportDataSource2.Value = this.tblDatosBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource2);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "WinAppMenu.ReportePorPrecio.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(232, 167);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(570, 265);
+            this.reportViewer1.TabIndex = 6;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.BackgroundImage = global::WinAppMenu.Properties.Resources.btnAzul;
+            this.btnMostrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMostrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMostrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnMostrar.Location = new System.Drawing.Point(670, 100);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(119, 34);
+            this.btnMostrar.TabIndex = 54;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrecio.Location = new System.Drawing.Point(492, 100);
+            this.txtPrecio.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(160, 34);
+            this.txtPrecio.TabIndex = 53;
+            this.txtPrecio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPrecio_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(41)))), ((int)(((byte)(110)))));
+            this.label3.Location = new System.Drawing.Point(237, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(249, 31);
+            this.label3.TabIndex = 52;
+            this.label3.Text = "Ingrese un precio:";
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tblDatosBindingSource
+            // 
+            this.tblDatosBindingSource.DataMember = "TblDatos";
+            this.tblDatosBindingSource.DataSource = this.dataSet1;
+            // 
             // FrmMaletasPorPrecio
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(932, 462);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.reportViewer1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FrmMaletasPorPrecio";
             this.Text = "FrmMaletasPorPrecio";
+            this.Load += new System.EventHandler(this.FrmMaletasPorPrecio_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblDatosBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -76,5 +153,11 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource tblDatosBindingSource;
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label3;
     }
 }
--- WinAppMenu/FrmMaletasPorPrecio.cs
+++ WinAppMenu/FrmMaletasPorPrecio.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,75 @@ namespace WinAppMenu
 {
     public partial class FrmMaletasPorPrecio : Form
     {
+        DataTable mochilasTable;
         public FrmMaletasPorPrecio()
         {
             InitializeComponent();
+            string rutaXml = Path.Combine(Application.StartupPath, "Mochilas.xml");
+            dataSet1.ReadXml(rutaXml);
+            mochilasTable = dataSet1.Tables[0];
+        }
+
+        private void FrmMaletasPorPrecio_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void txtPrecio_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                try
+                {
+                    float precio = float.Parse(txtPrecio.Text);
+                    if (precio < 0)
+                    {
+                        MessageBox.Show("Ingresar de nuevo. No hay precios negativos");
+                        txtPrecio.Clear();
+                    }
+                    else
+                    {
+                        btnMostrar.Focus();
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Valores no validos. Ingrese valores numericos");
+                    txtPrecio.Clear();
+                }
+            }
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float precio = float.Parse(txtPrecio.Text);
+                if (precio < 0)
+                {
+                    MessageBox.Show("Ingresar de nuevo. No hay precios negativos");
+                    txtPrecio.Clear();
+                }
+                else
+                {
+                    DataView vistaFiltrada = new DataView(mochilasTable);
+                    vistaFiltrada.RowFilter = $"Precio = '{precio}'";
+
+                    // Cargar el informe con los datos filtrados
+                    Microsoft.Reporting.WinForms.ReportDataSource rds =
+                        new Microsoft.Reporting.WinForms.ReportDataSource("DataSet1", vistaFiltrada);
+
+                    reportViewer1.LocalReport.DataSources.Clear();
+                    reportViewer1.LocalReport.DataSources.Add(rds);
+                    reportViewer1.RefreshReport();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Valores no validos. Ingrese valores númericos");
+                txtPrecio.Clear();
+            }
         }
     }
 }
